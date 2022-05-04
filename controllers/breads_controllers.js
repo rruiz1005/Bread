6a328dba2a52a0de73c02e915c663c0585b8f2c0
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-05-04 18:40:40 -0400

    create and new routes complete

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -13,6 +13,26 @@ breads.get('/', (req, res) => {
     //res.send(Bread)
 })
 
+// CREATE
+breads.post('/', (req, res) => {
+    if (!req.body.image) {
+        req.body.image = 'https://placekitten.com/200/300'
+    }
+    if(req.body.hasGluten === 'on') {
+        req.body.hasGluten === 'true'
+    } else {
+        req.body.hasGlutten === 'false'
+    }
+    Bread.push(req.body)
+    res.redirect(Bread)
+})
+
+// NEW
+breads.get('/new', (req, res) => {
+    res.render('new')
+})
+
+
 //show
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
